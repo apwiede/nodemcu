30b5800f6a914c2a76a33444bb552b07db658a7c
apwiede <arnulf@wiedemann-pri.de>
2016-08-16 19:20:12 +0200

    split off code ti structmsgTest.tcl.

--- tools/structmsgTest/structmsgCmd.tcl
+++ tools/structmsgTest/structmsgCmd.tcl
@@ -375,23 +375,3 @@ proc create_structmsg {numFieldInfos} {
   return $handle
 }
 
-set offset 0
-set handle [create_structmsg 5]
-set_targets $handle 123 456 789
-addField $handle "@randomNum" uint32_t 4
-addField $handle "pwd" uint8_t* 16
-set_fieldValue $handle "pwd" "/dir1/dir2/dir34"
-set_fillerAndCrc $handle
-#dump_structmsg $handle
-encodeMsg $handle
-dump_structmsg $handle
-#puts stderr "DICT:"
-#pdict [set ::structmsg($handle)]
-set encoded [getEncoded $handle]
-puts stderr "encoded: $encoded!"
-#set encoded [string replace $encoded 0 0 789]
-#set encoded [string replace $encoded 1 1 123]
-decodeMsg $handle $encoded
-dump_structmsg $handle
-#puts stderr "DICT:"
-#pdict [set ::structmsg($handle)]
