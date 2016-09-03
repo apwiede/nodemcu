0ec0704f36fbf16f3ce3101071d950ba8a56f9be
apwiede <arnulf@wiedemann-pri.de>
2016-09-03 22:00:00 +0200

    added example code to fill tablelist widget

--- tools/structmsgTest/structmsgTestDefinitions.tcl
+++ tools/structmsgTest/structmsgTestDefinitions.tcl
@@ -169,3 +169,166 @@ puts stderr "@totalLgth: value $value!"
 set result [::structmsg get_tableFieldValue $handle2 rssid1 1 value]
 checkErrOK $result
 puts stderr "rssid1 row 1: value $value!"
+
+
+package require Tk
+package require tablelist
+
+namespace eval apwWin {
+    variable ns [namespace current]
+
+    namespace ensemble create
+
+    namespace export CreateScrolledWidget Init CreateScrolledTablelist
+
+
+    proc Init {} {
+        variable infoWinNo
+        variable xPosShowEntryBox
+        variable yPosShowEntryBox
+
+        set infoWinNo 1
+        set xPosShowEntryBox -1
+        set yPosShowEntryBox -1
+    }
+
+    proc CreateScrolledWidget { wType w useAutoScroll titleStr args } {
+        variable ns
+        variable sApw
+
+        if { [winfo exists $w.par] } {
+            destroy $w.par
+        }
+        ttk::frame $w.par
+        pack $w.par -side top -fill both -expand 1
+        if { $titleStr ne "" } {
+            label $w.par.label -text "$titleStr" -anchor center
+            set sApw(bgDefaultColor) [$w.par.label cget -background]
+            set sApw(fgDefaultColor) [$w.par.label cget -foreground]
+        }
+        $wType $w.par.widget \
+               -xscrollcommand "$w.par.xscroll set" \
+               -yscrollcommand "$w.par.yscroll set" {*}$args
+        ttk::scrollbar $w.par.xscroll -command "$w.par.widget xview" -orient horizontal
+        ttk::scrollbar $w.par.yscroll -command "$w.par.widget yview" -orient vertical
+        set rowNo 0
+        if { $titleStr ne "" } {
+            set rowNo 1
+            grid $w.par.label -sticky ew -columnspan 2
+        }
+        grid $w.par.widget $w.par.yscroll -sticky news
+        grid $w.par.xscroll               -sticky ew
+
+        grid rowconfigure    $w.par $rowNo -weight 1
+        grid columnconfigure $w.par 0      -weight 1
+
+        bind $w.par.widget <MouseWheel>       "${ns}::MouseWheelCB $w.par.widget %D y"
+        bind $w.par.widget <Shift-MouseWheel> "${ns}::MouseWheelCB $w.par.widget %D x"
+
+        if { $useAutoScroll } {
+            autoscroll::autoscroll $w.par.xscroll
+            autoscroll::autoscroll $w.par.yscroll
+        }
+
+        return $w.par.widget
+    }
+
+    proc CreateScrolledTablelist { w useAutoScroll titleStr args } {
+        return [CreateScrolledWidget tablelist::tablelist $w $useAutoScroll $titleStr {*}$args]
+    }
+
+}
+
+namespace eval showDefinition {
+    variable ns [namespace current]
+
+    namespace ensemble create
+
+    namespace export Init CreateScrolledTablelist
+
+    # The following variables must be set, before reading parameters and
+    # before calling LoadSettings.
+    proc Init {} {
+        variable sApw
+
+        set sApw(tw)      ".apwDef" ; # Name of toplevel window
+        set sApw(appName) "apwDef"  ; # Name of tool
+        set sApw(cfgDir)  ""        ; # Directory containing config files
+
+        set sApw(startDir) [pwd]
+        set sApw(dir1)     $sApw(startDir)
+        set sApw(dir2)     $sApw(startDir)
+
+        set sApw(infoWinList) {}         ; # Start with empty file info window list
+        set sApw(leftFile)    ""         ; # Left  file for selective diff
+        set sApw(rightFile)   ""         ; # Right file for selective diff
+        set sApw(curSession)  "Default"  ; # Default session name
+        set sApw(sessionList) [list]
+        set sApw(curListbox)  ""
+
+        # Default values for command line options.
+        set sApw(optSync)             false
+        set sApw(optSyncDelete)       false
+        set sApw(optCopyDate)         false
+        set sApw(optCopyDays)         ""
+        set sApw(optSearch)           false
+        set sApw(optConvert)          false
+        set sApw(optConvertFmt)       ""
+        set sApw(optDiffOnStartup)    false
+        set sApw(optSessionOnStartup) ""
+        # Add command line options which should not be expanded by file matching.
+#        apwApps AddFileMatchIgnoreOption "filematch"
+    }
+
+    proc CreateScrolledTablelist { fr title } {
+        set id [apwWin CreateScrolledTablelist $fr true $title \
+                    -columns {50 "col1"   "left"
+                               0 "col2"   "right"
+                               0 "col3" "right" } \
+                    -exportselection false \
+                    -stretch 0 \
+                    -stripebackground #e0e8f0 \
+                    -selectmode extended \
+                    -labelcommand tablelist::sortByColumn \
+                    -showseparators true]
+        $id columnconfigure 0 -sortmode dictionary
+        $id columnconfigure 1 -sortmode integer
+        $id columnconfigure 2 -sortmode dictionary
+        return $id
+    }
+}
+
+source autoscroll.tcl
+
+apwWin Init
+showDefinition Init
+frame .tbl -width 600 -height 100
+set tableFr .tbl
+set tableId [showDefinition CreateScrolledTablelist $tableFr showDefinition]
+set lst [list]
+set valueLst [list]
+set rowLst [list]
+set row 0
+puts stderr "numTableRows: $numTableRows!"
+while {$row < $numTableRows} {
+  foreach name $tableFieldNames {
+    structmsg get_definitionTableFieldInfo $def $name 0 fieldInfo
+puts stderr "$name: $fieldInfo!"
+    if {$row == 0} {
+#      lappend lst [dict get $fieldInfo fieldLgth] $name
+      lappend lst 0 $name
+    }
+    set result [structmsg get_tableFieldValue $handle2 $name $row value]
+    lappend rowLst $value
+  }
+  lappend valueLst $rowLst
+  set rowLst [list]
+  incr row
+}
+$tableId configure -width 40
+$tableId configure -columns $lst
+puts stderr "valueLst: $valueLst!"
+foreach rowLst $valueLst {
+  $tableId insert end $rowLst
+}
+pack $tableFr
