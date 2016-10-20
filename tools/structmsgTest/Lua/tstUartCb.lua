7f4bb89080c7c4184846012261b08b1147fec465
apwiede <arnulf@wiedemann-pri.de>
2016-10-20 09:35:30 +0200

    now use structmsg instead of structmsg2

--- tools/structmsgTest/Lua/tstUartCb.lua
+++ tools/structmsgTest/Lua/tstUartCb.lua
@@ -1,17 +1,17 @@
 --print("heap0: "..node.heap())
-handle=structmsg2.createDispatcher()
-result=structmsg2.initDispatcher(handle)
+handle=structmsg.createDispatcher()
+result=structmsg.initDispatcher(handle)
 
 function sendIMsg(handle)
-  structmsg2.uartReceiveCb(handle,string.char(tonumber("57",16)),1)
-  structmsg2.uartReceiveCb(handle,string.char(tonumber("00",16)),1)
-  structmsg2.uartReceiveCb(handle,string.char(tonumber("4D",16)),1)
-  structmsg2.uartReceiveCb(handle,string.char(tonumber("00",16)),1)
-  structmsg2.uartReceiveCb(handle,string.char(tonumber("00",16)),1)
-  structmsg2.uartReceiveCb(handle,string.char(tonumber("08",16)),1)
-  structmsg2.uartReceiveCb(handle,string.char(73),1)
-  structmsg2.uartReceiveCb(handle,string.char(10),1)
---  structmsg2.uartReceiveCb(handle,string.char(tonumber("10",16)),1)
+  structmsg.uartReceiveCb(handle,string.char(tonumber("57",16)),1)
+  structmsg.uartReceiveCb(handle,string.char(tonumber("00",16)),1)
+  structmsg.uartReceiveCb(handle,string.char(tonumber("4D",16)),1)
+  structmsg.uartReceiveCb(handle,string.char(tonumber("00",16)),1)
+  structmsg.uartReceiveCb(handle,string.char(tonumber("00",16)),1)
+  structmsg.uartReceiveCb(handle,string.char(tonumber("08",16)),1)
+  structmsg.uartReceiveCb(handle,string.char(73),1)
+  structmsg.uartReceiveCb(handle,string.char(10),1)
+--  structmsg.uartReceiveCb(handle,string.char(tonumber("10",16)),1)
 end
   
   echo=0
@@ -21,7 +21,7 @@ end
   eol="\n"
   uart.on("data", 1,
     function(data)
-      structmsg2.uartReceiveCb(handle,data,1)
+      structmsg.uartReceiveCb(handle,data,1)
 --      sendIMsg(handle)
     end
   ,0)
