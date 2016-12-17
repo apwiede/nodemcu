f45cd472ecd9e2480711be5369e63adafd66682d
apwiede <arnulf@wiedemann-pri.de>
2016-12-17 18:39:13 +0100

    initial version.

---
+++ app/compMsg/compMsgSocket.c
@@ -0,0 +1,66 @@
+/*
+* Copyright (c) 2016, Arnulf P. Wiedemann (arnulf@wiedemann-pri.de)
+* All rights reserved.
+*
+* License: BSD/MIT
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* 1. Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the distribution.
+* 3. Neither the name of the copyright holder nor the names of its
+* contributors may be used to endorse or promote products derived
+* from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+
+/* 
+ * File:   compMsgSocket.c
+ * Author: Arnulf P. Wiedemann
+ *
+ * Created on December 17th, 2016
+ */
+
+#include "osapi.h"
+#include "c_types.h"
+#include "mem.h"
+#include "flash_fs.h"
+
+#include "lwip/err.h"
+#include "lwip/ip_addr.h"
+#include "espconn.h"
+#include "lwip/dns.h"
+
+#include "c_string.h"
+#include "c_stdlib.h"
+#include "c_stdio.h"
+#include "platform.h"
+#include "compMsgDispatcher.h"
+
+// ================================= newCompMsgSocket ====================================
+
+compMsgSocket_t *newCompMsgSocket() {
+  compMsgSocket_t *compMsgSocket = os_zalloc(sizeof(compMsgSocket_t));
+  if (compMsgSocket == NULL) {
+    return NULL;
+  }
+
+  return compMsgSocket;
+}
