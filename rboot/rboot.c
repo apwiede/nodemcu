961857709dc6f0ddd6e6d6de89bdc4f5ddc42428
apwiede <arnulf@wiedemann-pri.de>
2017-01-13 09:04:36 +0100

    fixes and some defines.

--- rboot/rboot.c
+++ rboot/rboot.c
@@ -289,6 +289,9 @@ static uint8_t default_config(rboot_config *romconf, uint32_t flashsize) {
   romconf->count = 2;
   romconf->roms[0] = SECTOR_SIZE * (BOOT_CONFIG_SECTOR + 1);
   romconf->roms[1] = (flashsize / 2) + (SECTOR_SIZE * (BOOT_CONFIG_SECTOR + 1));
+  romconf->user_rom_save_data_flag = BOOT_USER_ROM_NO_DATA_SET;
+  romconf->user_rom_save_data_size = 0;
+  romconf->user_rom_save_data[0] = '\0';
 }
 #endif
 
@@ -401,8 +404,8 @@ uint32_t NOINLINE find_image(void) {
   ets_printf("\r\n");
 
 #ifdef BOOT_AES128_CBC
-        // support for AES128 CBC encyption
-        test_encrypt_cbc();
+  // support for AES128 CBC encyption
+  test_encrypt_cbc();
 #endif
   // read boot config
   SPIRead(BOOT_CONFIG_SECTOR * SECTOR_SIZE, buffer, SECTOR_SIZE);
@@ -447,14 +450,31 @@ uint32_t NOINLINE find_image(void) {
 #endif
 
 ets_printf("romconf size: %d\n", sizeof(rboot_config));
-ets_printf("user_save_flag: %d len: %d\n", romconf->user_rom_save_data_flag, romconf->user_rom_save_data_size);
-if (romconf->user_rom_save_data_flag) {
-ets_printf("user_save_data: %s\n", romconf->user_rom_save_data);
-  // clear user_save_data
-  romconf->user_rom_save_data_flag = 0;
-  ets_memset(romconf->user_rom_save_data, '\0', romconf->user_rom_save_data_size);
-  romconf->user_rom_save_data_size = 0;
-}
+ets_printf("user_rom_save_data_flag: %d len: %d\n", romconf->user_rom_save_data_flag, romconf->user_rom_save_data_size);
+  switch (romconf->user_rom_save_data_flag) {
+  case BOOT_USER_ROM_NO_DATA_SET:
+    break;
+  case BOOT_USER_ROM_DATA_SET:
+    break;
+  case BOOT_USER_ROM_DO_SAVE_DATA:
+    break;
+  case BOOT_USER_ROM_NO_SAVE_DATA:
+    break;
+  case BOOT_USER_ROM_RESET_DATA:
+    // clear user_save_data
+    romconf->user_rom_save_data_flag = 0;
+    ets_memset(romconf->user_rom_save_data, '\0', romconf->user_rom_save_data_size);
+    romconf->user_rom_save_data_size = 0;
+    updateConfig = TRUE;
+    break;
+  case BOOT_USER_ROM_DATA_RESETTED:
+    break;
+  case BOOT_USER_ROM_DATA_SAVED:
+    break;
+  default:
+    break;
+  }
+  
 #ifdef BOOT_GPIO_ENABLED
   if (perform_gpio_boot(romconf)) {
     if (romconf->gpio_rom >= romconf->count) {
