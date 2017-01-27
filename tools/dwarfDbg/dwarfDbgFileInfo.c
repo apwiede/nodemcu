bda706f868589ef94018fb0958f5bb62c19609e7
apwiede <arnulf@wiedemann-pri.de>
2017-01-27 10:31:21 +0100

    initial version.

---
+++ tools/dwarfDbg/dwarfDbgFileInfo.c
@@ -0,0 +1,312 @@
+/*
+* Copyright (c) 2017, Arnulf P. Wiedemann (arnulf@wiedemann-pri.de)
+* All rights reserved.
+*
+* License: BSD/MIT
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* 1. Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the distribution.
+* 3. Neither the name of the copyright holder nor the names of its
+* contributors may be used to endorse or promote products derived
+* from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+
+/* 
+ * File:   dwarfDbgFileInfo.c
+ * Author: Arnulf P. Wiedemann <arnulf@wiedemann-pri.de>
+ *
+ * Created on January 27, 2017
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <libelf.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "dwarfDbgInt.h"
+
+// =================================== addDirName =========================== 
+
+static uint8_t addDirName(dwarfDbgPtr_t self, char *dirName) {
+  uint8_t result;
+
+  result = DWARF_DBG_ERR_OK;
+  if (self->dwarfDbgFileInfo->dirNamesInfo.maxDirName <= self->dwarfDbgFileInfo->dirNamesInfo.numDirName) {
+    self->dwarfDbgFileInfo->dirNamesInfo.maxDirName += 50;
+    if (self->dwarfDbgFileInfo->dirNamesInfo.dirNames == NULL) {
+      self->dwarfDbgFileInfo->dirNamesInfo.dirNames = (char **)ckalloc(sizeof(char *) * self->dwarfDbgFileInfo->dirNamesInfo.maxDirName);
+      if (self->dwarfDbgFileInfo->dirNamesInfo.dirNames == NULL) {
+        return DWARF_DBG_ERR_OUT_OF_MEMORY;
+      }
+    } else {
+      self->dwarfDbgFileInfo->dirNamesInfo.dirNames = (char **)ckrealloc((char *)self->dwarfDbgFileInfo->dirNamesInfo.dirNames, sizeof(char *) * self->dwarfDbgFileInfo->dirNamesInfo.maxDirName);
+      if (self->dwarfDbgFileInfo->dirNamesInfo.dirNames == NULL) {
+        return DWARF_DBG_ERR_OUT_OF_MEMORY;
+      }
+    }
+  }
+  self->dwarfDbgFileInfo->dirNamesInfo.dirNames[self->dwarfDbgFileInfo->dirNamesInfo.numDirName] = (char *)ckalloc(strlen(dirName) + 1);
+  if (self->dwarfDbgFileInfo->dirNamesInfo.dirNames[self->dwarfDbgFileInfo->dirNamesInfo.numDirName] == NULL) {
+    return DWARF_DBG_ERR_OUT_OF_MEMORY;
+  }
+  self->dwarfDbgFileInfo->dirNamesInfo.dirNames[self->dwarfDbgFileInfo->dirNamesInfo.numDirName][strlen(dirName)] = '\0';
+  memcpy(self->dwarfDbgFileInfo->dirNamesInfo.dirNames[self->dwarfDbgFileInfo->dirNamesInfo.numDirName], dirName, strlen(dirName));
+//printf("addDirName: %d %s\n", self->dwarfDbgFileInfo->dirNamesInfo.numDirName, dirName);
+  self->dwarfDbgFileInfo->dirNamesInfo.numDirName++;
+  return result;
+}
+
+// =================================== addFileName =========================== 
+
+static uint8_t addFileName(dwarfDbgPtr_t self, char *fileName, size_t dirNameIdx) {
+  uint8_t result;
+  fileNameInfo_t *fileNameInfo;
+
+  result = DWARF_DBG_ERR_OK;
+  if (self->dwarfDbgFileInfo->fileNamesInfo.maxFileName <= self->dwarfDbgFileInfo->fileNamesInfo.numFileName) {
+    self->dwarfDbgFileInfo->fileNamesInfo.maxFileName += 50;
+    if (self->dwarfDbgFileInfo->fileNamesInfo.fileNames == NULL) {
+      self->dwarfDbgFileInfo->fileNamesInfo.fileNames = (fileNameInfo_t *)ckalloc(sizeof(fileNameInfo_t) * self->dwarfDbgFileInfo->fileNamesInfo.maxFileName);
+      if (self->dwarfDbgFileInfo->fileNamesInfo.fileNames == NULL) {
+        return DWARF_DBG_ERR_OUT_OF_MEMORY;
+      }
+    } else {
+      self->dwarfDbgFileInfo->fileNamesInfo.fileNames = (fileNameInfo_t *)ckrealloc((char *)self->dwarfDbgFileInfo->fileNamesInfo.fileNames, sizeof(fileNameInfo_t) * self->dwarfDbgFileInfo->fileNamesInfo.maxFileName);
+      if (self->dwarfDbgFileInfo->fileNamesInfo.fileNames == NULL) {
+        return DWARF_DBG_ERR_OUT_OF_MEMORY;
+      }
+    }
+  }
+  fileNameInfo = &self->dwarfDbgFileInfo->fileNamesInfo.fileNames[self->dwarfDbgFileInfo->fileNamesInfo.numFileName];
+  fileNameInfo->fileName = (char *)ckalloc(strlen(fileName) + 1);
+  if (fileNameInfo->fileName == NULL) {
+    return DWARF_DBG_ERR_OUT_OF_MEMORY;
+  }
+  fileNameInfo->fileName[strlen(fileName)] == '\0';
+  memcpy(fileNameInfo->fileName, fileName, strlen(fileName));
+  fileNameInfo->dirNameIdx = dirNameIdx;
+//printf("addFileName: num: %d dirNameIdx: %d %s\n", self->dwarfDbgFileInfo->fileNamesInfo.numFileName, dirNameIdx, fileName);
+  self->dwarfDbgFileInfo->fileNamesInfo.numFileName++;
+  return result;
+}
+
+// =================================== addSourceFile =========================== 
+
+static uint8_t addSourceFile(dwarfDbgPtr_t self, char *pathName, size_t compileUnitIdx, size_t *fileNameIdx, size_t *fileInfoIdx) {
+  uint8_t result;
+  char *cp;
+  int i;
+  int dirIdx;
+  int fileIdx;
+  fileNameInfo_t *fileNameInfo;
+
+  result = DWARF_DBG_ERR_OK;
+  cp = strrchr(pathName, '/');
+  *cp++ = '\0';
+printf("path: %s name: %s\n", pathName, cp);
+  dirIdx = -1;
+  for (i = 0; i < self->dwarfDbgFileInfo->dirNamesInfo.numDirName; i++) {
+    if (strcmp(pathName, self->dwarfDbgFileInfo->dirNamesInfo.dirNames[i]) == 0) {
+printf("found: dirName %d num: %d\n", i, self->dwarfDbgFileInfo->dirNamesInfo.numDirName);
+      dirIdx = i;
+      break;
+    }
+  }
+  if (dirIdx < 0) {
+    dirIdx = self->dwarfDbgFileInfo->dirNamesInfo.numDirName;
+    result = self->dwarfDbgFileInfo->addDirName(self, pathName);
+    checkErrOK(result);
+  }
+  fileIdx = -1;
+  for (i = 0; i < self->dwarfDbgFileInfo->fileNamesInfo.numFileName; i++) {
+    fileNameInfo = &self->dwarfDbgFileInfo->fileNamesInfo.fileNames[i];
+    if ((strcmp(cp, fileNameInfo->fileName) == 0) && (fileNameInfo->dirNameIdx == dirIdx)) {
+printf("found: fileName %d num: %d\n", i, self->dwarfDbgFileInfo->fileNamesInfo.numFileName);
+      fileIdx = i;
+      break;
+    }
+  }
+  if (fileIdx < 0) {
+    fileIdx = self->dwarfDbgFileInfo->fileNamesInfo.numFileName;
+    result = self->dwarfDbgFileInfo->addFileName(self, cp, dirIdx);
+    checkErrOK(result);
+  }
+  *fileNameIdx = fileIdx;
+  result = self->dwarfDbgFileInfo->addFileInfo(self, compileUnitIdx, fileIdx, fileInfoIdx);
+  checkErrOK(result);
+  return result;
+}
+
+// =================================== addFileInfo =========================== 
+
+static uint8_t addFileInfo(dwarfDbgPtr_t self, size_t compileUnitIdx, size_t fileNameIdx, size_t *fileInfoIdx) {
+  uint8_t result;
+  compileUnit_t *compileUnit;
+  compileUnitInfo_t *compileUnitInfo;
+  fileInfo_t *fileInfo;
+
+  result = DWARF_DBG_ERR_OK;
+  compileUnit = &self->dwarfDbgGetInfo->compileUnits[compileUnitIdx];
+  compileUnitInfo = &compileUnit->compileUnitInfo;
+  if (compileUnitInfo->maxFileInfo <= compileUnitInfo->numFileInfo) {
+    compileUnitInfo->maxFileInfo += 10;
+    if (compileUnitInfo->fileInfos == NULL) {
+      compileUnitInfo->fileInfos = (fileInfo_t *)ckalloc(sizeof(fileInfo_t) * compileUnitInfo->maxFileInfo);
+      if (compileUnitInfo->fileInfos == NULL) {
+        return DWARF_DBG_ERR_OUT_OF_MEMORY;
+      }
+    } else {
+      compileUnitInfo->fileInfos = (fileInfo_t *)ckrealloc((char *)compileUnitInfo->fileInfos, sizeof(fileInfo_t) * compileUnitInfo->maxFileInfo);
+      if (compileUnitInfo->fileInfos == NULL) {
+        return DWARF_DBG_ERR_OUT_OF_MEMORY;
+      }
+    }
+  }
+  fileInfo = &compileUnitInfo->fileInfos[compileUnitInfo->numFileInfo];
+  fileInfo->maxFileLine = 0;
+  fileInfo->numFileLine = 0;
+  fileInfo->fileLines = NULL;
+  fileInfo->fileNameIdx = fileNameIdx;
+  *fileInfoIdx = compileUnitInfo->numFileInfo;
+printf("addFileInfo: compileUnitIdx: %d fileNameIdx: %d fileInfoIdx: %d\n", compileUnitIdx, fileNameIdx, *fileInfoIdx);
+  compileUnitInfo->numFileInfo++;
+  return result;
+}
+
+// =================================== addFileLine =========================== 
+
+static uint8_t addFileLine(dwarfDbgPtr_t self, Dwarf_Addr pc, size_t lineNo, size_t compileUnitIdx, size_t fileInfoIdx, size_t *fileLineIdx) {
+  uint8_t result;
+  compileUnit_t *compileUnit;
+  compileUnitInfo_t *compileUnitInfo;
+  fileInfo_t *fileInfo;
+  fileLineInfo_t *fileLineInfo;
+
+printf("addFileLine: pc: 0x%08x lineNo: %d\n", pc, lineNo);
+  result = DWARF_DBG_ERR_OK;
+  compileUnit = &self->dwarfDbgGetInfo->compileUnits[compileUnitIdx];
+  compileUnitInfo = &compileUnit->compileUnitInfo;
+  if (compileUnitInfo->fileInfos == NULL) {
+    // seems to be no file infos!!
+    return result;
+  }
+  fileInfo = &compileUnitInfo->fileInfos[fileInfoIdx];
+  if (fileInfo->maxFileLine <= fileInfo->numFileLine) {
+    fileInfo->maxFileLine += 5;
+    if (fileInfo->fileLines == NULL) {
+      fileInfo->fileLines = (fileLineInfo_t *)ckalloc(sizeof(fileLineInfo_t) * fileInfo->maxFileLine);
+      if (fileInfo->fileLines == NULL) {
+        return DWARF_DBG_ERR_OUT_OF_MEMORY;
+      }
+    } else {
+      fileInfo->fileLines = (fileLineInfo_t *)ckrealloc((char *)fileInfo->fileLines, sizeof(fileLineInfo_t) * fileInfo->maxFileLine);
+      if (fileInfo->fileLines == NULL) {
+        return DWARF_DBG_ERR_OUT_OF_MEMORY;
+      }
+    }
+  }
+  fileLineInfo = &fileInfo->fileLines[fileInfo->numFileLine];
+  fileLineInfo->lineNo = lineNo;
+  fileLineInfo->pc = pc;
+  *fileLineIdx = fileInfo->numFileLine;
+  fileInfo->numFileLine++;
+  return result;
+}
+
+// =================================== addCompileUnitFile =========================== 
+
+static uint8_t addCompileUnitFile(dwarfDbgPtr_t self, char *fileName, Dwarf_Off overallOffset, size_t *compileUnitIdx) {
+  uint8_t result;
+  compileUnitInfo_t *compileUnitInfo;
+
+printf("addCompileUnitFile\n");
+  result = DWARF_DBG_ERR_OK;
+  if (self->dwarfDbgFileInfo->compileUnitsInfo.maxCompileUnit <= self->dwarfDbgFileInfo->compileUnitsInfo.numCompileUnit) {
+    self->dwarfDbgFileInfo->compileUnitsInfo.maxCompileUnit += 50;
+    if (self->dwarfDbgFileInfo->compileUnitsInfo.compileUnitInfos == NULL) {
+      self->dwarfDbgFileInfo->compileUnitsInfo.compileUnitInfos = (compileUnitInfo_t *)ckalloc(sizeof(compileUnitInfo_t) * self->dwarfDbgFileInfo->compileUnitsInfo.maxCompileUnit);
+      if (self->dwarfDbgFileInfo->compileUnitsInfo.compileUnitInfos == NULL) {
+        return DWARF_DBG_ERR_OUT_OF_MEMORY;
+      }
+    } else {
+      self->dwarfDbgFileInfo->compileUnitsInfo.compileUnitInfos = (compileUnitInfo_t *)ckrealloc((char *)self->dwarfDbgFileInfo->compileUnitsInfo.compileUnitInfos, sizeof(compileUnitInfo_t) * self->dwarfDbgFileInfo->compileUnitsInfo.maxCompileUnit);
+      if (self->dwarfDbgFileInfo->compileUnitsInfo.compileUnitInfos == NULL) {
+        return DWARF_DBG_ERR_OUT_OF_MEMORY;
+      }
+    }
+  }
+  compileUnitInfo = &self->dwarfDbgFileInfo->compileUnitsInfo.compileUnitInfos[self->dwarfDbgFileInfo->compileUnitsInfo.numCompileUnit];
+  compileUnitInfo->fileName = (char *)ckalloc(strlen(fileName) + 1);
+  if (compileUnitInfo->fileName == NULL) {
+    return DWARF_DBG_ERR_OUT_OF_MEMORY;
+  }
+  compileUnitInfo->fileName[strlen(fileName)] == '\0';
+  compileUnitInfo->maxFileInfo = 0;
+  compileUnitInfo->numFileInfo = 0;
+  compileUnitInfo->fileInfos = NULL;
+  memcpy(compileUnitInfo->fileName, fileName, strlen(fileName));
+  compileUnitInfo->overallOffset = overallOffset;
+printf("addCompileUnit: num: %d overallOffset: %d %s\n", self->dwarfDbgFileInfo->compileUnitsInfo.numCompileUnit, overallOffset, fileName);
+  *compileUnitIdx = self->dwarfDbgFileInfo->compileUnitsInfo.numCompileUnit;
+  self->dwarfDbgFileInfo->compileUnitsInfo.numCompileUnit++;
+  return result;
+}
+
+// =================================== dwarfDbgGetFileInfos =========================== 
+
+int dwarfDbgGetFileInfos(dwarfDbgPtr_t self) {
+  uint8_t result;
+
+printf("dwarfDbgGetFileInfos\n");
+  result = DWARF_DBG_ERR_OK;
+
+  return result;
+}
+
+// =================================== dwarfDbgFileInfoInit =========================== 
+
+int dwarfDbgFileInfoInit (dwarfDbgPtr_t self) {
+  self->dwarfDbgFileInfo->dirNamesInfo.maxDirName = 0;
+  self->dwarfDbgFileInfo->dirNamesInfo.numDirName = 0;
+  self->dwarfDbgFileInfo->dirNamesInfo.dirNames = NULL;
+
+  self->dwarfDbgFileInfo->fileNamesInfo.maxFileName = 0;
+  self->dwarfDbgFileInfo->fileNamesInfo.numFileName = 0;
+  self->dwarfDbgFileInfo->fileNamesInfo.fileNames = NULL;
+
+  self->dwarfDbgFileInfo->compileUnitsInfo.maxCompileUnit = 0;
+  self->dwarfDbgFileInfo->compileUnitsInfo.numCompileUnit = 0;
+  self->dwarfDbgFileInfo->compileUnitsInfo.compileUnitInfos = NULL;
+
+  self->dwarfDbgFileInfo->addDirName = addDirName;
+  self->dwarfDbgFileInfo->addFileName = addFileName;
+  self->dwarfDbgFileInfo->addSourceFile = addSourceFile;
+  self->dwarfDbgFileInfo->addFileLine = addFileLine;
+  self->dwarfDbgFileInfo->addFileInfo = addFileInfo;
+  self->dwarfDbgFileInfo->addCompileUnitFile = addCompileUnitFile;
+  return DWARF_DBG_ERR_OK;
+}
+
+
---
+++ tools/dwarfDbg/dwarfDbgFileInfo.h
@@ -0,0 +1,109 @@
+/*
+* Copyright (c) 2017, Arnulf P. Wiedemann (arnulf@wiedemann-pri.de)
+* All rights reserved.
+*
+* License: BSD/MIT
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* 1. Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the distribution.
+* 3. Neither the name of the copyright holder nor the names of its
+* contributors may be used to endorse or promote products derived
+* from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+
+/* 
+ * File:   dwarfDbgFileInfo.h
+ * Author: Arnulf P. Wiedemann <arnulf@wiedemann-pri.de>
+ *
+ * Created on January 27, 2017
+ */
+
+#ifndef DWARFDBG_FILE_INFO_H
+#define	DWARFDBG_FILE_INFO_H
+
+typedef uint8_t (* addDirName_t)(dwarfDbgPtr_t self, char *dirName);
+typedef uint8_t (* addFileName_t)(dwarfDbgPtr_t self, char *fileName, size_t dirNameIdx);
+typedef uint8_t (* addSourceFile_t)(dwarfDbgPtr_t self, char *pathName, size_t compileUnitIdx, size_t *fileNameIdx, size_t *fileInfoIdx);
+typedef uint8_t (* addFileInfo_t)(dwarfDbgPtr_t self, size_t compileUnitIdx, size_t fileNameIdx, size_t *fileInfoIdx);
+typedef uint8_t (* addFileLine_t)(dwarfDbgPtr_t self, Dwarf_Addr pc, size_t lineNo, size_t compileUnitIdx, size_t fileInfoIdx, size_t *fileLineIdx);
+typedef uint8_t (* addCompileUnitFile_t)(dwarfDbgPtr_t self, char *pathName, Dwarf_Off overallOffset, size_t *compileUnitIdx);
+
+typedef struct dirNamesInfo {
+  int  maxDirName;    /* Size of the dirNames array. */
+  int  numDirName;    /* Index of the topmost dirName */
+  char **dirNames;
+} dirNamesInfo_t;
+
+typedef struct fileNameInfo {
+  char *fileName;
+  size_t dirNameIdx;
+} fileNameInfo_t;
+  
+typedef struct fileNamesInfo {
+  int  maxFileName;    /* Size of the fileNames array. */
+  int  numFileName;    /* Index of the topmost fileName */
+  fileNameInfo_t *fileNames;
+} fileNamesInfo_t;
+
+typedef struct fileLineInfo {
+  Dwarf_Addr pc;
+  size_t lineNo;
+} fileLineInfo_t;
+
+typedef struct fileInfo {
+  size_t fileNameIdx;
+  int  maxFileLine;    /* Size of the fileLines array. */
+  int  numFileLine;    /* Index of the topmost fileName */
+  fileLineInfo_t *fileLines;
+} fileInfo_t;
+
+typedef struct compileUnitInfo {
+  char *fileName;
+  Dwarf_Off overallOffset;
+  int  maxFileInfo;    /* Size of the fileInfos array. */
+  int  numFileInfo;    /* Index of the topmost fileName */
+  fileInfo_t *fileInfos;
+} compileUnitInfo_t;
+
+typedef struct compileUnitsInfo {
+  int  maxCompileUnit;    /* Size of the compileUnits array. */
+  int  numCompileUnit;    /* Index of the topmost fileName */
+  compileUnitInfo_t *compileUnitInfos;
+} compileUnitsInfo_t;
+
+typedef struct dwarfDbgFileInfo {
+  dirNamesInfo_t dirNamesInfo;
+  fileNamesInfo_t fileNamesInfo;
+  compileUnitsInfo_t compileUnitsInfo;
+
+  addDirName_t addDirName;
+  addFileName_t addFileName;
+  addSourceFile_t addSourceFile;
+  addFileInfo_t addFileInfo;
+  addFileLine_t addFileLine;
+  addCompileUnitFile_t addCompileUnitFile;
+ 
+} dwarfDbgFileInfo_t;
+
+
+#endif  /* DWARFDBG_FILE_INFO_H */
