3cdad46c37312db29e863b80f385f5b83d2ae291
apwiede <arnulf@wiedemann-pri.de>
2017-04-02 21:40:40 +0200

    initial version.

---
+++ module_image_files/buildSpiffsImg.sh
@@ -0,0 +1,29 @@
+set -x
+#if [ -f ../myConfig.txt ]
+#then
+#  cat CompMsgWifiValues.txt ../myConfig.txt > myConfig.txt
+#else
+#  echo "missing file ../myConfig.txt aborting!"
+#fi
+spiffsimg -f ../bin/spiffs1.img -c 0x60000 -i <<EOF
+import startCompMsgA.lua startCompMsgA.lua
+import startCompMsgC.lua startCompMsgC.lua
+import startCompMsgD.lua startCompMsgD.lua
+import startCompMsgG.lua startCompMsgG.lua
+import startCompMsgI.lua startCompMsgI.lua
+import startCompMsgL.lua startCompMsgL.lua
+import startCompMsgN.lua startCompMsgN.lua
+import startCompMsgO.lua startCompMsgO.lua
+import startCompMsgo.lua startCompMsgo.lua
+import startCompMsgS.lua startCompMsgS.lua
+import startCompMsgU.lua startCompMsgU.lua
+import startCompMsgW.lua startCompMsgW.lua
+
+import MsgUse.txt MsgUse.txt
+import MsgDescHeader.txt MsgDescHeader.txt
+import MsgDescMidPart.txt MsgDescMidPart.txt
+import MsgDescTrailer.txt MsgDescTrailer.txt
+import MsgValHeader1.txt MsgValHeader1.txt
+
+EOF
+#rm -rf myConfig.txt
