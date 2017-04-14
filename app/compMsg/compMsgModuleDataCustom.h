6d3dd241e94d5464a500502f36924f56562e43fa
apwiede <arnulf@wiedemann-pri.de>
2017-04-14 10:24:43 +0200

    initial version.

---
+++ app/compMsg/compMsgModuleDataCustom.h
@@ -0,0 +1,35 @@
+// has to be bigger than COMP_MSG_WIFI_SSDPStatus !!
+
+#define COMP_MSG_MODULE_DATA_PingState               50
+#define COMP_MSG_MODULE_DATA_DeviceSecurity          51
+#define COMP_MSG_MODULE_DATA_ProgRunningMode         52
+#define COMP_MSG_MODULE_DATA_CurrentRunningMode      53
+#define COMP_MSG_MODULE_DATA_Region                  54
+#define COMP_MSG_MODULE_DATA_SerieNumber             55
+#define COMP_MSG_MODULE_DATA_HdrReserve              56
+#define COMP_MSG_MODULE_DATA_PasswdC                 57
+#define COMP_MSG_MODULE_DATA_OperatingMode           58
+#define COMP_MSG_MODULE_DATA_MyU16Src                59
+#define COMP_MSG_MODULE_DATA_MyU8Src                 60
+#define COMP_MSG_MODULE_DATA_MyU16SaveUserDataCmdKey 61
+#define COMP_MSG_MODULE_DATA_MyU8SaveUserDataCmdKey  62
+#define COMP_MSG_MODULE_DATA_SrcId                   63
+#define COMP_MSG_MODULE_DATA_GUID                    64
+
+  {"@getPingState", COMP_MSG_MODULE_DATA_PingState},
+  {"@getDeviceSecurity", COMP_MSG_MODULE_DATA_DeviceSecurity},
+  {"@getProgRunningMode", COMP_MSG_MODULE_DATA_ProgRunningMode},
+  {"@getCurrentRunningMode", COMP_MSG_MODULE_DATA_CurrentRunningMode},
+  {"@getRegion", COMP_MSG_MODULE_DATA_Region},
+  {"@getSerieNumber", COMP_MSG_MODULE_DATA_SerieNumber},
+  {"@getHdrReserve", COMP_MSG_MODULE_DATA_HdrReserve},
+  {"@getPasswdC", COMP_MSG_MODULE_DATA_PasswdC},
+  {"@setOperatingMode", COMP_MSG_MODULE_DATA_OperatingMode},
+  {"@getMyU16Src", COMP_MSG_MODULE_DATA_MyU16Src},
+  {"@getMyU8Src", COMP_MSG_MODULE_DATA_MyU8Src},
+  {"@getMyU16SaveUserDataCmdKey", COMP_MSG_MODULE_DATA_MyU16SaveUserDataCmdKey},
+  {"@getMyU8SaveUserDataCmdKey", COMP_MSG_MODULE_DATA_MyU8SaveUserDataCmdKey},
+
+  {"@getSrcId", COMP_MSG_MODULE_DATA_SrcId},
+  {"@getGUID", COMP_MSG_MODULE_DATA_GUID},
+
